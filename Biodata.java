1862d37d5290d866b977e809c8e09e50ad36e47a
bilgisroif64-png <bilgisroif64@gmail.com>
2026-09-16 16:37:35 +0700

    Add Biodata class with personal information output

---
+++ Biodata.java
@@ -0,0 +1,9 @@
+  public class Biodata {
+    public static void main(String[] args) {
+        System.out.println("Nama     : Bilgis Ro'if Sabilian Syah");
+        System.out.println("NIM      : 261301013");
+        System.out.println("Alamat   : Ngrandu Kedungadem Bojonegoro");
+        System.out.println("Jurusan  : Sistem Komputer");
+        System.out.println("Universitas: Universitas Nahdlatul Ulama");
+        }
+}
